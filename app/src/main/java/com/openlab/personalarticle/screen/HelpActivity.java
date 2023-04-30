c365be0e1985f6696b63a76806927fd7ff826250
umayanga1997 <umayanga.avi@gmail.com>
2023-04-30 20:12:58 +0530

    Added help video

--- app/src/main/java/com/openlab/personalarticle/screen/HelpActivity.java
+++ app/src/main/java/com/openlab/personalarticle/screen/HelpActivity.java
@@ -3,8 +3,12 @@ package com.openlab.personalarticle.screen;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.MenuItem;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 import com.openlab.personalarticle.R;
 
@@ -15,6 +19,21 @@ public class HelpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        VideoView videoView =(VideoView)findViewById(R.id.helpVideoView);
+
+        //Creating MediaController
+        MediaController mediaController= new MediaController(this);
+        mediaController.setAnchorView(videoView);
+
+        //specify the location of media file
+        Uri uri=Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.help);
+
+        //Setting MediaController and URI, then starting the videoView
+        videoView.setMediaController(mediaController);
+        videoView.setVideoURI(uri);
+        videoView.requestFocus();
+        videoView.start();
     }
 
     @Override
